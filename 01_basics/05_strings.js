7e0866985603e7ce71d83884a7e3a300929eb564
Piyush kumar <89901717+Piyush-k-r@users.noreply.github.com>
2024-10-26 17:52:01 +0530

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,1 +1,8 @@
+const name = "piyush"
+const repoCount = 50
+
+// console.log(name + repoCount + " value"); => old way 
+// new way also called as string interpolation
+
+console.log('Hello my name is ${name} and my repo count is ${repoCount}');
 
